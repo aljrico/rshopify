c89f24b64b15db968530033c9df80d0887ef0e46
aljrico <aljrico@gmail.com>
2020-11-09 17:21:18 +0000

    stable testing

--- tests/testthat/test-init.R
+++ tests/testthat/test-init.R
@@ -1,11 +1,20 @@
-testthat::test_that("connection can be made", {
-  # Read locally saved credentials
-  credentials <- readRDS('shop_credentials')
+
+# Read locally saved credentials
+credentials <- readRDS('../testdata/shop_credentials')
+
+
+test_that("connection can be made", {
   
   # Establish connection
-  ShopifyConnection$new(
+  sc <<- ShopifyConnection$new(
     shop_url =  credentials$shop_url,
     api_key = credentials$api_key,
     api_password = credentials$api_password
   )
+})
+test_that("Orders can be downloaded", {
+  
+  # Download all pending orders
+  sc$get_orders()
+  
 })
